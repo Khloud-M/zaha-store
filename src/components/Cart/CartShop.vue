e7bcd12914370eb7e66d4f31b791e9843543ed46
Khloud <62654747+Khloud-M@users.noreply.github.com>
2023-03-13 16:55:15 +0200

    update cart

--- src/components/Cart/CartShop.vue
+++ src/components/Cart/CartShop.vue
@@ -1,12 +1,29 @@
 <template>
-
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-  <div class="offcanvas-header">
-    <h5 id="offcanvasRightLabel">Offcanvas right</h5>
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  <div
+    class="offcanvas offcanvas-end"
+    tabindex="-1"
+    id="offcanvasRight"
+    aria-labelledby="offcanvasRightLabel"
+  >
+    <div class="offcanvas-header">
+      <h5 id="offcanvasRightLabel">My Cart</h5>
+      <button
+        type="button"
+        class="btn-close text-reset"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+      ></button>
+    </div>
+    <div class="offcanvas-body">
+      <empty-cart />
+    </div>
   </div>
-  <div class="offcanvas-body">
-    ...
-  </div>
-</div>
 </template>
+<script>
+import EmptyCart from "@/components/Cart/EmptyCart.vue";
+export default {
+  components: {
+    EmptyCart,
+  },
+};
+</script>
