71555eeb1f7e25112523e47a686ce13500b2cc56
Khloud <62654747+Khloud-M@users.noreply.github.com>
2023-11-02 13:34:02 +0200

    testupadates

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@
 <script>
 import TheHeader from "./components/layout/TheHeader.vue";
 import TheFooter from "./components/layout/TheFooter.vue";
+
 export default {
   components: {
     TheHeader,
--- src/main.js
+++ src/main.js
@@ -1,58 +1,59 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import Vuex from 'vuex'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Vuex from "vuex";
 // import ui component and public component
-import BaseButton from './components/ui/BaseButton.vue'
-import LandingPage from './components/layout/LandingPage.vue'
-import ProductItem from '@/components/Products/PorductItem.vue';
-import CartShop from './components/Cart/CartShop.vue';
+import BaseButton from "./components/ui/BaseButton.vue";
+import LandingPage from "./components/layout/LandingPage.vue";
+import ProductItem from "@/components/Products/PorductItem.vue";
+import CartShop from "./components/Cart/CartShop.vue";
 
-// import bootstap 
-import bootstrap from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+// import bootstap
+import bootstrap from "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 // animation library
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init();
 // Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 import "vuetify/dist/vuetify.min.css";
-
 
 const vuetify = createVuetify({
   components,
   directives,
-})
+});
 // import toast tontifaction
-import ToastPlugin from 'vue-toast-notification';
-// import css file 
-import "@/assets/style/main.css"
-import "@/assets/style/form.css"
+import ToastPlugin from "vue-toast-notification";
+// import css file
+import "@/assets/style/main.css";
+import "@/assets/style/form.css";
 // import axios
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import axios from "axios";
+import VueAxios from "vue-axios";
 // axios.defaults.baseURL='https://backend.zahaaksa.com/api';
 import VOtpInput from "vue3-otp-input";
-
+import { createHead } from "@vueuse/head";
+const head = createHead();
 
 createApp(App)
-.use(router)
-.use(store)
-.use(Vuex)
-.use(bootstrap)
-.use(vuetify)
-.use(ToastPlugin)
-.use(VueAxios, axios)
-.component("bace-button", BaseButton)
-.component("landing-page", LandingPage)
-.component("product-item", ProductItem)
-.component("cart-shop", CartShop)
-.component('v-otp-input', VOtpInput)
-.mount('#app')
+  .use(router)
+  .use(store)
+  .use(Vuex)
+  .use(bootstrap)
+  .use(vuetify)
+  .use(ToastPlugin)
+  .use(VueAxios, axios)
+  .use(head)
+  .component("bace-button", BaseButton)
+  .component("landing-page", LandingPage)
+  .component("product-item", ProductItem)
+  .component("cart-shop", CartShop)
+  .component("v-otp-input", VOtpInput)
+  .mount("#app");
